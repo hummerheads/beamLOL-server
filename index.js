68384a49dea81528fd9463af096f31815d748404
hummerheads <mdabdullah.allinfozone@gmail.com>
2024-11-05 15:12:12 +0600

    hit backend

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function run() {
       try {
         const user = await allUsersCollection.findOne({ telegram_ID });
         if (user) {
-          res.send({ balance: user.balance });
+          res.send({ balance: user.balance, perk: user.perk, level: user.level, bonus: user.bonus, spin: user.spin, available_energy: user.available_energy, total_energy: user.total_energy });
         } else {
           res.status(404).send({ message: "User not found" });
         }
